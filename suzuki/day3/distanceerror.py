27f490831d9a686123134f517795fa10151d7e4c
SuzukiAym <suzuki.ayumu.ez@mynavi.jp>
2024-05-17 08:21:30 +0000

    day3の変更

---
+++ suzuki/day3/distanceerror.py
@@ -0,0 +1,16 @@
+import sys
+args = sys.argv
+sta1 = args[1]
+sta2 = args[2]
+dis = {"東京":0.00, "品川":6.78, "新横浜":25.54, "名古屋":342.02, "京都":476.31, "新大阪":515.35}
+try:
+    if dis[sta1] > dis[sta2]:
+        A = dis[sta1] - dis[sta2]
+        truncated_number_round = round(A, 2)
+        print(truncated_number_round, end="")
+    else:
+        A = dis[sta2] - dis[sta1]
+        truncated_number_round = round(A, 2)
+        print(truncated_number_round, end="")
+except:
+    print("のぞみの停車駅を引数に設定してください", end="")
---
+++ suzuki/day3/func_oddeven.py
@@ -0,0 +1,10 @@
+import funk
+import sys
+args = sys.argv
+num1 = int(args[1])
+num2 = int(args[2])
+num3 = int(args[3])
+  
+funk.calcvalue(num1)
+funk.calcvalue(num2)
+funk.calcvalue(num3)
